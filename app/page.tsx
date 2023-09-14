dca186e946a75d6be42d1d84b93c23080afd12eb
ruudada <ruud.ada@gmail.com>
2023-09-14 10:37:31 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -27,10 +27,6 @@ type ProjectSearch = {
   };
 };
 
-export const dynamic = "force-dynamic";
-export const dynamicParams = true;
-export const revalidate = 0;
-
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
   const data = (await fetchAllProjects(category, endcursor)) as ProjectSearch;
 
